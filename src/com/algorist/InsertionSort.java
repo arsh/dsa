8395469085ece87408457ca963c0881c48f0f128
andres <andres.santana@gmail.com>
2014-03-06 12:21:48 -0800

    adding insertion sort from top of my head

---
+++ src/com/algorist/InsertionSort.java
@@ -0,0 +1,33 @@
+package com.algorist;
+
+/**
+ * Just testing if I can implement this algorithm from the top of my head
+ * 
+ * @author andres
+ * 
+ */
+public class InsertionSort {
+	
+	
+	public static void main(String[] args) {
+		System.out.println(sort("selectionsort"));
+	}
+	
+
+	public static String sort(String toSort) {
+		char[] chars = toSort.toCharArray();
+
+		for (int i = 0; i < chars.length; i++) {
+			for (int j = i + 1; j < chars.length; j++) {
+				if (chars[j] < chars[i]) {
+					// swap
+					char min = chars[j];
+					chars[j] = chars[i];
+					chars[i] = min;
+				}
+			}
+		}
+
+		return new String(chars);
+	}
+}
